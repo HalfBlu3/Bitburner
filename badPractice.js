9dc0af6542bbcb6f0effaeb26b4e3b476e27de92
HalfBlu3 <73621901+HalfBlu3@users.noreply.github.com>
2023-04-04 15:13:14 -0700

    deleted test junk

--- badPractice.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-	while (ture){
-		ns.run("ccfinder.js", 1);
-		while (ns.isRunning("ccfinder.js", ns.getHostname())) await ns.sleep(10);
-	}
-}
